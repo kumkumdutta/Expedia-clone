77e5792e3c1fa8df4405587202ee9fae3fb0d72b
amityadav11 <amityadav11@hotmail.com>
2023-03-29 19:19:13 +0530

    Saty

---
+++ src/Pages/city.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        id: "1",
+        city: "Mumbai",
+    },
+    {
+        id: "2",
+        city: "Kolkota",
+    },
+    {
+        id: "3",
+        city: "Bhagalpur",
+    }
+]
+
+export default data;
