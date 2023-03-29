191e9c176756dc693cfdd4c307f36e13fd18cdf6
amityadav11 <amityadav11@hotmail.com>
2023-03-29 20:19:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import HomePage from './Pages/HomePage';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Stay/>
-=======
+     
       <HomePage/>
       
->>>>>>> ba7519c697ceab9740bdb366a72ac30ccb0769a0
     </div>
   );
 }
