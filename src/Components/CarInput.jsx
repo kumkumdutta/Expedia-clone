a3383b067b82272fd3c8d211b1323948643a3594
KumkumDutta <112754510+kumkumdutta@users.noreply.github.com>
2023-03-29 11:26:41 +0530

    files and folders ctreated

---
+++ src/Components/CarInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box, Input } from '@chakra-ui/react'
+
+export const CarInput = () => {
+  return (
+    <Box>
+        <Input placeholder='Pick up' size='md' />
+        <Input placeholder='Pick up' size='md' />
+
+    </Box>
+    
+    
+    
+ 
+  )
+}
