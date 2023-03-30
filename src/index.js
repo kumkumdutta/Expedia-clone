951c007dcf44324096fb8eb2e130d40db1b2754d
Ashish Mishra <ashishmishra9467@gmail.com>
2023-03-30 21:24:25 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraBaseProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
   <ChakraBaseProvider>
      <App />
   </ChakraBaseProvider>
-  </BrowserRouter>
    
  
 );
