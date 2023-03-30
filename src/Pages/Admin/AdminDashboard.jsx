2ddf3cb12b0d90d97da35bf0bd7fae75df858e7a
sagarbalsaraf <sagarbalsaraf004@gmail.com>
2023-03-30 17:25:14 +0530

    add functional admin part

--- src/Pages/Admin/AdminDashboard.jsx
+++ src/Pages/Admin/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchFlightProducts } from "../../Redux/AdminFlights/action";
 import "./AdminDashboard.Module.css";
@@ -11,12 +12,14 @@ export const AdminDashboard = () => {
   const [flight, setFlight] = useState(0);
   const [hotel, setHotel] = useState(0);
   const [users, setUsers] = useState(0);
+  const [giftCard, setGiftCard] = useState(0);
+  const [things, setThings] = useState(0);
  const [loading, setLoading] = useState(false);
 
   const getHotel = () => {
     setLoading(true);
     axios
-      .get("") //https://makemytrip-api-data.onrender.com/flight
+      .get("http://localhost:8080/flight")
       .then((res) => {
         setFlight(res.data.length);
       })
@@ -25,7 +28,7 @@ export const AdminDashboard = () => {
       });
     //
     axios
-      .get("") //https://makemytrip-api-data.onrender.com/hotel
+      .get("http://localhost:8080/hotel")
       .then((res) => {
         setHotel(res.data.length);
       })
@@ -34,13 +37,32 @@ export const AdminDashboard = () => {
       });
     //
     axios
-      .get("") //https://makemytrip-api-data.onrender.com/users
+      .get("http://localhost:8080/users")
       .then((res) => {
         setUsers(res.data.length);
       })
       .catch((err) => {
         console.log(err);
       });
+
+      axios
+      .get("http://localhost:8080/giftcards")
+      .then((res) => {
+        setGiftCard(res.data.length);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    
+      axios
+      .get("http://localhost:8080/Things_todo")
+      .then((res) => {
+        setThings(res.data.length);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    
     
   };
 
@@ -55,6 +77,8 @@ export const AdminDashboard = () => {
           <h1><Link to={"/admin"}>Home</Link></h1>
           <h1><Link to={"/admin/adminflight"}>Add Flight</Link></h1>
           <h1><Link to={"/admin/adminstay"}>Add Stays</Link></h1>
+          <h1><Link to={"/admin/products"}>All Flights</Link></h1>
+          <h1><Link to={"/admin/hotels"}>All Hotels</Link></h1>
           <h1><Link to={"/"}>Log out</Link></h1>
         </div>
         <div className="mainBox">
@@ -68,18 +92,28 @@ export const AdminDashboard = () => {
             {/*  */}
             <div className="dataBx">
               <h1>Total Hotel</h1>
-              {loading ? <h1>Loading...</h1> : <h1>{hotel}</h1>}
+              {<h1>{hotel}</h1>}
               <Link to="/admin/hotels">View</Link>
             </div>
             <div className="dataBx">
               <h1>Total Flights</h1>
-              {loading ? <h1>Loading...</h1> : <h1>{flight}</h1>}
+              {<h1>{flight}</h1>}
               <Link to="/admin/flights">View</Link>
             </div>
             <div className="dataBx">
               <h1>Total Users</h1>
-              {loading ? <h1>Loading...</h1> : <h1>{users}</h1>}
+              {<h1>{users}</h1>}
               <Link to="/admin">View</Link>
+            </div>
+            <div className="dataBx">
+              <h1>Giftcards</h1>
+              {<h1>{giftCard}</h1>}
+              <Link to="/admin/giftcards">View</Link>
+            </div>
+            <div className="dataBx">
+              <h1>Pakages Available</h1>
+              {<h1>{things}</h1>}
+              <Link to="/setThings">View</Link>
             </div>
             {/*  */}
           </div>
--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,9 +1,11 @@
 import { Admin } from "./Admin/AdminFlight";
-import { AdminHotel, AdminStay } from "./Admin/AdminStay";
+import { AdminStay } from "./Admin/AdminStay";
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 // import { HomePage } from "./HomePage";
 import { AdminDashboard } from "./Admin/AdminDashboard";
+import { AdminProducts } from "./Admin/AdminProducts";
+import { AllHotels } from "./Admin/AllHotels";
 
 export const AllRoutes = () => {
     return (
@@ -11,8 +13,10 @@ export const AllRoutes = () => {
           <Routes>
             {/* <Route path="/" element={<HomePage />} /> */}
             <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/admin/adminstay" element={<AdminStay />} />
             <Route path="/admin/adminflight" element={<Admin />} />
+            <Route path="/admin/adminstay" element={<AdminStay />} />
+            <Route path="/admin/products" element={<AdminProducts />} />
+            <Route path="/admin/hotels" element={<AllHotels />} />
           </Routes>
         </>
       );
