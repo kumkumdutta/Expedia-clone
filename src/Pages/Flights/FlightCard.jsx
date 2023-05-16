760cc001a040792b039c47c6a77aaf0694a31631
Ashish Mishra <ashishmishra9467@gmail.com>
2023-05-17 00:37:56 +0530

    done

--- src/Pages/Flights/FlightCard.jsx
+++ src/Pages/Flights/FlightCard.jsx
@@ -1,47 +1,62 @@
-import { Box, Image, Flex, Button } from "@chakra-ui/react"
-import axios from "axios"
-import { useToast } from "@chakra-ui/react"
-import { Link } from "react-router-dom"
+import { Box, Image, Flex, Button } from "@chakra-ui/react";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function FlightCard({ data }) {
-  const { id, airline, from, to, departure, arrival, price, totalTime } = data
-  const toast = useToast()
+  const { id, airline, from, to, departure, arrival, price, totalTime } = data;
+  const toast = useToast();
 
   const handleClick = () => {
-    axios.post(`http://localhost:8000/flightcart`, data)
+    axios.post(`http://localhost:8000/flightcart`, data);
     //   .then((res) => console.log(res))
     //   .catch((err) => console.log(err))
 
     toast({
-      title: 'Flight Add to Cart',
+      title: "Flight Add to Cart",
       description: "Please Proceed to Payment",
-      status: 'success',
+      status: "success",
       duration: 9000,
       isClosable: true,
-    })
-  }
+    });
+  };
 
-  const Booknow={
+  
+
+  const Booknow = {
     marginTop: "3%",
     // width:"164px",
-    padding:"15px",
+    padding: "15px",
     height: "43px",
     background: "teal",
-    color:" #FFFFFF",
+    color: " #FFFFFF",
     bordeRadius: "0.5rem",
     position: "relative",
-    marginBottom:"1rem"
-}
-
+    marginBottom: "1rem",
+  };
 
   return (
-    <Box display={"flex"}
-      gap="20px" key={id} height="100px"
-      width={"80%"} boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
-      padding="10px" margin="auto" justifyContent="space-around"
-      alignItems={"center"} borderRadius="10px" marginBottom={"20px"} textAlign="center">
+    <Box
+      display={"flex"}
+      gap="20px"
+      key={id}
+      height="100px"
+      width={"80%"}
+      boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+      padding="10px"
+      margin="auto"
+      justifyContent="space-around"
+      alignItems={"center"}
+      borderRadius="10px"
+      marginBottom={"20px"}
+      textAlign="center"
+    >
       <Box gap={"30px"}>
-        <Image src="https://play-lh.googleusercontent.com/OhZSLjRDLvFLqtDp9bIgcvAweZIg5V5uIMI_7kOaS-9nPR043DUfoibkn1BgwG7Ai1U=w240-h480-rw" width={"35px"} height="30px" />
+        <Image
+          src="https://play-lh.googleusercontent.com/OhZSLjRDLvFLqtDp9bIgcvAweZIg5V5uIMI_7kOaS-9nPR043DUfoibkn1BgwG7Ai1U=w240-h480-rw"
+          width={"35px"}
+          height="30px"
+        />
         <h1>{airline}</h1>
       </Box>
       <Flex display={"flex"} flexDirection="column">
@@ -55,7 +70,7 @@ export default function FlightCard({ data }) {
         <b style={{ fontSize: "14px" }}>{to} </b>
       </Flex>
       <Flex display={"flex"} flexDirection="column">
-        <h3 >Duation</h3>
+        <h3>Duation</h3>
         <b>{totalTime}</b>
       </Flex>
       <Flex display={"flex"} flexDirection="column">
@@ -63,9 +78,10 @@ export default function FlightCard({ data }) {
         <b>{price}</b>
       </Flex>
       <Link to={"/checkout"}>
-        <Button style={Booknow} onClick={handleClick}>Book Now</Button>
+        <Button style={Booknow} onClick={handleClick}>
+          Book Now
+        </Button>
       </Link>
     </Box>
-
-  )
+  );
 }
